54cfe1ce680154f1d69005bedb50d8494908609b
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-24 13:42:06 +0300

    Create task3.java

---
+++ Chapter2/task3.java
@@ -0,0 +1,10 @@
+/**
+ * 3. Найдите фрагмент кода ATypeName и сделайте из него программу, пригодную для компиляции и запуска.
+ */
+
+public class ATypeName {
+
+    public static void main(String[] args) {
+        ATypeName aTypeName = new ATypeName();
+    }
+}
