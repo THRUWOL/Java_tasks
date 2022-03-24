f0f98f8e8a180ef9cf3c1761d5b9a5c63a0cbbb4
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-24 13:44:13 +0300

    Create task4.java

---
+++ Chapter2/task4.java
@@ -0,0 +1,9 @@
+/*
+ *Сделайте то же для кода с использованием класса DataOnly.
+ */
+
+public class DataOnly {
+    public static void main(String[] args) {
+        DataOnly dataOnly = new DataOnly();
+    }
+}
