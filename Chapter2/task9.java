a493deb3d9c98738feb9dad70285641c06a8fd8e
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-25 10:56:32 +0300

    Create task9.java

---
+++ Chapter2/task9.java
@@ -0,0 +1,15 @@
+/*
+*Напишите программу, демонстрирующую автоматическую упаковку примиитивных типов.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Integer i = new Integer(45);
+        System.out.println(i);
+
+        int j = new Integer(45);
+        System.out.println(j);
+
+        System.out.println(j+i);
+    }
+}
