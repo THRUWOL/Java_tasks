bca54411bf1eddceef1ce8e30f7c1e9619e68af8
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-25 12:26:06 +0300

    Update task1.java

--- Chapter3/task1.java
+++ Chapter3/task1.java
@@ -2,6 +2,8 @@
 * Напишите программу, в которой используются как «короткие», так и «длинные» команды печати.
 */
 
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Длинная команда печати");
