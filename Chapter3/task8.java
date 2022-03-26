d8625b16e4dec6fa0fc2c4f864355851426c6448
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-26 14:03:15 +0300

    Update task8.java

--- Chapter3/task8.java
+++ Chapter3/task8.java
@@ -6,9 +6,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 0x264ab54f;
-        int b = 222225642;
+        // 10011001001010101101011111111111110100001000110010001101001111
+        long a = 0x264ab5fff423234fL;
         System.out.println(Long.toBinaryString(a));
+        
+        // 1111011010111000010100111000100100111110001100101101110010011
+        long b = 2222256422245522323L;
         System.out.println(Long.toBinaryString(b));
     }
 }
