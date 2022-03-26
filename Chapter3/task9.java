b630bf79411187047dec91e3b6992969fcfd8f6f
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-03-26 14:46:22 +0300

    Create task9.java

---
+++ Chapter3/task9.java
@@ -0,0 +1,13 @@
+/*
+* Выведите наибольшее и наименьшее число в экспоненциальной записи для 
+* типов float и double.
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Float\n max: " + Float.MAX_VALUE + "\n min: " + Float.MIN_VALUE);
+
+        System.out.println("Double\n max: " + Double.MAX_VALUE + "\n min: " + Double.MIN_VALUE);
+    }
+}
